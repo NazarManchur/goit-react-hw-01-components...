62e10eadd551a4dfd23707d90bf3274f8939cfdc
NazarManchur <126466634+NazarManchur@users.noreply.github.com>
2023-11-28 20:11:28 +0200

    1

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -1,12 +1,16 @@
+import { getQueriesForElement } from "@testing-library/react"
 import Friends from "../friends.json"
+import css from "../../src/components/style.css"
 
 
-export const Friend = () => {
+
+export const Friend = (variant) => {
     return (
         <ul className="friend-list">
             {Friends.map((el) => (
                 <li className="item">
-                    <span className="status">{ }</span>
+                    <span className="status"
+                    >{}</span>
                     <img className="avatar" src={el.avatar} alt="User avatar" width="48" />
                     <p className="name">{el.name}</p>
                 </li>))}
